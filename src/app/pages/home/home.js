dd16524fc20118028af6af691bcbcd61610b3210
Tarique Ejaz <tariqueejaz.93102@gmail.com>
2019-07-23 04:26:45 +0700

    dynamic links addition

--- src/app/pages/home/home.js
+++ src/app/pages/home/home.js
@@ -1,29 +1,31 @@
 import React, { Component } from 'react';
 import LinkedIn from '../../assets/images/linkedin-logo.svg';
 import GitHub from '../../assets/images/github-logo.svg';
+import { HomePage } from '../../data/pf-data.json';
 import './home.scss';
 
 class Home extends Component {
     render() {
+        const { FullName, BannerTitle, BannerDescription, WorkLinks } = HomePage;
         console.log(this.props);
         return (
             <div className="home-container">
                 <div className="home-detail">
                     <div className="home-detail_name">
-                        <p className="home-detail_name--first">Tarique</p>
-                        <p className="home-detail_name--last">Ejaz</p>
-                        <p className="home-detail_title">Full Stack Software Development Engineer</p>
-                        <p className="home-detail_description">Fast paced, highly enthusiastic, believes that the world is multiple shades of grey rather than one. Loves a challenge and makes sure to deliver solutions to interesting real-world problems.</p>
+                        <p className="home-detail_name--first">{FullName.substring(0, FullName.indexOf(' '))}</p>
+                        <p className="home-detail_name--last">{FullName.substring(FullName.lastIndexOf(' '), FullName.length)}</p>
+                        <p className="home-detail_title">{BannerTitle}</p>
+                        <p className="home-detail_description">{BannerDescription}</p>
                     </div>
                     <div className="home-detail_social">
-                        <a href="#">
+                        <a href={WorkLinks.LinkedIn}>
                             <img
                                 src={LinkedIn}
                                 alt="linkedin"
                                 className="home-detail_social--img"
                             />
                         </a>
-                        <a href="#">
+                        <a href={WorkLinks.GitHub}>
                             <img
                                 src={GitHub}
                                 alt="github"
