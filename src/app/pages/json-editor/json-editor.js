171a953ae2b832207572657b1789e78d5e1d1de8
Tarique Ejaz <Tarique.Ejaz@agoda.com>
2019-08-02 17:47:38 +0700

    made changes to json-editor form section

--- src/app/pages/json-editor/json-editor.js
+++ src/app/pages/json-editor/json-editor.js
@@ -46,14 +46,16 @@ class JsonEditor extends Component {
                     className="jsonedit_form"
                     style={{ backgroundColor: this.props.secondaryColor }}
                 >
-                    <p className="jsonedit_form--title">Form Your Portfolio</p>
-                    <p className="jsonedit_form--caption">The JSON data can be edited directly to reflect upon the portfolio. It is a simple one-stop data source, changes made will easily reflect on the main site.<br />As far as the images are concerned, it is preferred if you have a hosted image whose url can be easily shared in the JSON data. You can also upload the home page display image below. Please try to provide hosted links for the other images, as much as is feasible for creating a more seamless experience.<br/><i><b>(Note: The main display image should have a transparent background in order to conform with the design pattern of the portfolio)</b></i></p>
+                    <div className="jsonedit_form_container">
+                        <p className="jsonedit_form--title">Form Your Portfolio</p>
+                        <p className="jsonedit_form--caption">The JSON data can be edited directly to reflect upon the portfolio. It is a simple one-stop data source, changes made will easily reflect on the main site.<br /><br/>As far as the images are concerned, it is preferred if you have a hosted image whose url can be easily shared in the JSON data. You can also upload the home page display image below. Please try to provide hosted links for the other images, as much as is feasible for creating a more seamless experience.<br /><br/><b>(Note: The main display image should have a transparent background in order to conform with the design pattern of the portfolio)</b><br/></p>
 
-                    <input
-                        type="file"
-                        className="resume_form--input"
-                        onChange={(e) => this.uploadDisplayImage(e)}
-                    />
+                        <input
+                            type="file"
+                            className="resume_form--input"
+                            onChange={(e) => this.uploadDisplayImage(e)}
+                        />
+                    </div>
                 </div>
             </div>
         );
