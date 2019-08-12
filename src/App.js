cc5eb6f203acf58134a294ce9899e0f7879f1363
Tarique Ejaz <tariqueejaz.93102@gmail.com>
2019-08-13 01:18:16 +0700

    home page responsive styling

--- src/App.js
+++ src/App.js
@@ -35,10 +35,10 @@ class App extends Component {
     return (
       <Grid fluid style={{ margin: 0, padding: 0, overflow: "hidden" }}>
         <Row>
-          <Col xs={0} lg={1} md={1} style={{ padding: 0 }}>
+          <Col xs={0} lg={1} md={0} style={{ padding: 0 }}>
             <NavBar primaryColor={this.state.primaryColor} secondaryColor={this.state.secondaryColor} mainJsonData={this.state.mainJsonData} />
           </Col>
-          <Col xs={12} lg={11} md={11} style={{ padding: 0 }}>
+          <Col xs={12} lg={11} md={12} style={{ padding: 0 }}>
             <Routes primaryColor={this.state.primaryColor} secondaryColor={this.state.secondaryColor} changeSecondaryColor={this.changeSecondaryColor} mainJsonData={this.state.mainJsonData} modifyMainJsonData={this.modifyMainJsonData}
             />
           </Col>
