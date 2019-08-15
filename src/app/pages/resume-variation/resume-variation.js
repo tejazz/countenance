30c43e01243baf98cdb14f1a0ebfb256ccef3f8a
Tarique Ejaz <Tarique.Ejaz@agoda.com>
2019-08-15 15:36:00 +0700

    add path for varied resumes

---
+++ src/app/pages/resume-variation/resume-variation.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './resume-variation.scss';
+
+class ResumeVariation extends Component {
+    render() {
+        return (
+            <div>
+                Resume Variation
+            </div>
+        );
+    }
+}
+
+export default ResumeVariation;
