787997d3d02fe02c11e7bcce88cb3dcfc9044ac3
Tarique Ejaz <tariqueejaz.93102@gmail.com>
2019-08-18 23:26:36 +0700

    minor changes

--- src/app/pages/template-engine/template-engine.js
+++ src/app/pages/template-engine/template-engine.js
@@ -16,12 +16,12 @@ class TemplateEngine extends Component {
         })
             .then((canvas) => {
                 const imgData = canvas.toDataURL('image/png');
-                const pdf = new jsPDF("p", "mm", "a4");
+                const pdf = new jsPDF("p", "mm", "a4", true);
 
                 let width = pdf.internal.pageSize.getWidth();
                 let height = pdf.internal.pageSize.getHeight();
 
-                pdf.addImage(imgData, 'PNG', 0, 0, width, height);
+                pdf.addImage(imgData, 'PNG', 0, 0, width, height, '', 'FAST');
                 pdf.save("countenance-resume.pdf");
             });
 
